75f890950ae31d592a0fc8eb61f661794ee7b5b7
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-11-08 01:06:23 +0100

    chore: add missing files/entries from template

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,27 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   slug: 'hydra',
   name: 'Hydra',
+  version: "1.0.0",
   description: "Hydra gives you access to all the UGent info wherever you are",
-  icon: "./assets/logo/ugent.png",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  ios: {
+    supportsTablet: false
+  },
+  android: {
+  package: "be.ugent.zeus.hydra",
+    adaptiveIcon: {
+      foregroundImage: "./assets/icon.png",
+      backgroundColor: "#ffffff"
+    }
+  },
 });
 
--- flake.nix
+++ flake.nix
@@ -5,57 +5,62 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     devshell.url = "github:numtide/devshell";
     flake-utils.url = "github:numtide/flake-utils";
-    android.url = "github:tadfisher/android-nixpkgs/stable";
   };
   outputs = { self, flake-utils, devshell, nixpkgs, android }:
-    {
-
-      overlay = final: prev: {
-        inherit (self.packages.${final.system}) android-sdk android-studio;
-      };
-    }
-    // flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let 
         inherit (nixpkgs) lib;
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
-          overlays = [ devshell.overlays.default self.overlay ];
+          config.android_sdk.accept_license = true;
+          overlays = [ devshell.overlays.default ];
         };
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          toolsVersion = "26.1.1";
+          platformToolsVersion = "33.0.3";
+          buildToolsVersions = [ "30.0.3" ];
+          includeEmulator = true;
+          emulatorVersion = "31.3.14";
+          platformVersions = [ "33" ];
+          includeSources = false;
+          includeSystemImages = false;
+          systemImageTypes = [ "google_apis_playstore" ];
+          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+          cmakeVersions = [ "3.18.1" ];
+          includeNDK = true;
+          ndkVersions = ["22.1.7171670"];
+          useGoogleAPIs = false;
+          useGoogleTVAddOns = false;
+          includeExtras = [
+            "extras;google;gcm"
+          ];
+        };
+        androidRootSdk = "${androidComposition.androidsdk}/libexec/android-sdk";
       in
       {
-        packages = {
-          android-sdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
-              # Useful packages for building and testing.
-              build-tools-34-0-0
-              cmdline-tools-latest
-              emulator
-              platform-tools
-              platforms-android-34
-              system-images-android-34-google-apis-x86-64
-            ]
-            ++ lib.optionals (system == "aarch64-darwin") [
-              system-images-android-34-google-apis-arm64-v8a
-              system-images-android-34-google-apis-playstore-arm64-v8a
-            ]
-            ++ lib.optionals (system == "x86_64-darwin" || system == "x86_64-linux") [
-              system-images-android-34-google-apis-x86-64
-              system-images-android-34-google-apis-playstore-x86-64
-            ]);
-          } // {
-            android-studio = pkgs.androidStudioPackages.stable;
-          };
         devShells.default = pkgs.devshell.mkShell {
           name = "hydra-react-native";
-          packages = with pkgs; [jdk11 gradle git android-sdk android-studio nodejs nodePackages.yarn ];
+          packages = with pkgs; [
+            jdk11 gradle git androidComposition.androidsdk
+            nodejs nodePackages.yarn android-studio
+          ];
           env = with pkgs; [
             {
               name = "ANDROID_HOME";
-              value = "${android-sdk}/share/android-sdk";
+              value = "${androidComposition.androidsdk}/libexec/android-sdk";
             }
             {
               name = "ANDROID_SDK_ROOT";
-              value = "${android-sdk}/share/android-sdk";
+              value = androidRootSdk;
+            }
+            {
+              name = "ANDROID_NDK_ROOT";
+              value = "${androidRootSdk}/ndk-bundle";
+            }
+            {
+              name = "GRADLE_OPTS";
+              value = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidRootSdk}/build-tools/33.0.1/aapt2";
             }
             {
               name = "JAVA_HOME";
