e9113738aa939f1377119a9514b13fdac9e39045
draxaris1010 <axel.dezittere@gmail.com>
2023-11-30 20:52:57 +0100

    Added language detection
     The language will be English by default. This will be replaced by the settings in the app.

--- src/stores/news.ts
+++ src/stores/news.ts
@@ -2,6 +2,7 @@ import {UGentArticle} from "../types/stores";
 import {ENDPOINTS} from "../constant";
 import {useSuspenseQuery} from "@tanstack/react-query";
 import {useFocusNotifyOnChangeProps} from "../lib/hooks/useFocusNotifyOnChangeProps";
+import {NativeModules, Platform} from "react-native";
 
 export const useUGentQuery = () => {
   const notifyOnChangeProps = useFocusNotifyOnChangeProps();
@@ -9,7 +10,14 @@ export const useUGentQuery = () => {
   return useSuspenseQuery<UGentArticle[], Error>({
     queryKey: ["news"],
     queryFn: async () => {
-      const res = await fetch(`${ENDPOINTS.HYDRA_V2}/news/nl.json`);
+
+      // This will later be managed by the settings in the app
+      var language = 'en';
+      if((Platform.OS == "android" && NativeModules.I18nManager.localeIdentifier.startsWith("nl")) 
+      || (Platform.OS == "ios" && NativeModules.SettingsManager.settings.AppleLocale.startsWith("nl"))) {
+        language = 'nl';
+      }
+      const res = await fetch(`${ENDPOINTS.HYDRA_V2}/news/` + language + `.json`);
       const json = await res.json()
       return json.entries;
     },
