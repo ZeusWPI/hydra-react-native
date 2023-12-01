74d562948f2c3485a4984373763c94a6062e02ad
squaredetector <117200367+squaredetector@users.noreply.github.com>
2023-12-01 02:17:59 +0100

    kleurcode aangepast, verschil achtergrond en logo fixen

--- app.config.ts
+++ app.config.ts
@@ -11,7 +11,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   userInterfaceStyle: "automatic",
   splash: {
     image: "./assets/icon.png",
-    backgroundColor: "#0063CF",
+    backgroundColor: "#1e64c8",
   },
   assetBundlePatterns: ["**/*"],
   ios: {
